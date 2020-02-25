cbd518be95c789eaa918ee55498924ef4c463ab9
Christophe Maymard <christophe.maymard@hotmail.com>
2020-02-25 11:46:10 +0100

    [C++] Create Tag class.

---
+++ src/PhpCode/Language/Cpp/Lexical/Tag.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of the PhpCode library.
+ * 
+ * @copyright   2020, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpCode\Language\Cpp\Lexical;
+
+/**
+ * Represents all the tags that identify tokens.
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class Tag
+{
+}
+
