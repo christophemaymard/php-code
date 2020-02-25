cce4729ad3ce8cc79268d7fe3e4c61c25190a3a5
Christophe Maymard <christophe.maymard@hotmail.com>
2020-02-25 12:08:21 +0100

    Create InvalidValueException class.

---
+++ src/PhpCode/Exception/InvalidValueException.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file is part of the PhpCode library.
+ * 
+ * @copyright   2020, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpCode\Exception;
+
+/**
+ * Represents the exception thrown when a value does not adhere to a defined 
+ * valid data domain.
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class InvalidValueException extends \Exception implements PhpCodeExceptionInterface
+{
+}
+
