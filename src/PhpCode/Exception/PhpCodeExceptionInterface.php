9d63077161fbbf20c4b1c9fba96d7e983d73d174
Christophe Maymard <christophe.maymard@hotmail.com>
2020-02-25 12:07:28 +0100

    Create PhpCodeExceptionInterface interface.

---
+++ src/PhpCode/Exception/PhpCodeExceptionInterface.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of the PhpCode library.
+ * 
+ * @copyright   2020, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpCode\Exception;
+
+/**
+ * Interface for all the exceptions.
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+interface PhpCodeExceptionInterface extends \Throwable
+{
+}
+
