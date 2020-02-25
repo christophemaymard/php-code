d9a136c4e159acae999f132707d0d95644a7f8d5
Christophe Maymard <christophe.maymard@hotmail.com>
2020-02-25 17:39:22 +0100

    [C++] Create LanguageContextInterface interface.

---
+++ src/PhpCode/Language/Cpp/LanguageContextInterface.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * This file is part of the PhpCode library.
+ * 
+ * @copyright   2020, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpCode\Language\Cpp;
+
+use PhpCode\Language\Cpp\Lexical\TokenTableInterface;
+
+/**
+ * Represents the interface for a language context.
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+interface LanguageContextInterface
+{
+    /**
+     * Returns the table of keyword tokens.
+     * 
+     * @return  TokenTableInterface
+     */
+    public function getKeywords(): TokenTableInterface;
+}
+
