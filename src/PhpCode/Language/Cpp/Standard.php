c49dc1fe931eb51bc18bf8935c5910eff365fe20
Christophe Maymard <christophe.maymard@hotmail.com>
2020-03-01 07:01:29 +0100

    [C++] Create Standard class.

---
+++ src/PhpCode/Language/Cpp/Standard.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * This file is part of the PhpCode library.
+ * 
+ * @copyright   2020, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpCode\Language\Cpp;
+
+/**
+ * Represents all the standards.
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class Standard
+{
+    /**
+     * C++ 2003.
+     * @var int
+     */
+    public const CPP2003 = 1;
+    
+    /**
+     * C++ 2011.
+     * @var int
+     */
+    public const CPP2011 = 2;
+    
+    /**
+     * C++ 2014.
+     * @var int
+     */
+    public const CPP2014 = 4;
+    
+    /**
+     * C++ 2017.
+     * @var int
+     */
+    public const CPP2017 = 8;
+}
+
