9f39a83e3ab93a494c2d9b9f1a169869b01c5393
Christophe Maymard <christophe.maymard@hotmail.com>
2020-03-05 18:48:38 +0100

    [C++] Create ParametersAndQualifiers class.

---
+++ src/PhpCode/Language/Cpp/Declarator/ParametersAndQualifiers.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This file is part of the PhpCode library.
+ * 
+ * @copyright   2020, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpCode\Language\Cpp\Declarator;
+
+/**
+ * Represents parameters and qualifiers of a function.
+ * 
+ * parameters-and-qualifiers:
+ *     ( )
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class ParametersAndQualifiers
+{
+}
+
