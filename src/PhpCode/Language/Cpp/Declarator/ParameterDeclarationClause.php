31958d9c6abf59c80485b862687aae93dbd5acf4
Christophe Maymard <christophe.maymard@hotmail.com>
2020-03-07 16:41:08 +0100

    [C++] Parameter declaration clause can have a parameter declaration list.

--- src/PhpCode/Language/Cpp/Declarator/ParameterDeclarationClause.php
+++ src/PhpCode/Language/Cpp/Declarator/ParameterDeclarationClause.php
@@ -11,12 +11,19 @@ namespace PhpCode\Language\Cpp\Declarator;
  * Represents a parameter declaration clause.
  * 
  * parameter-declaration-clause:
- *     ...[opt]
+ *     parameter-declaration-list[opt] ...[opt]
+ *     parameter-declaration-list , ...
  * 
  * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
  */
 class ParameterDeclarationClause
 {
+    /**
+     * The parameter declaration list.
+     * @var ParameterDeclarationList|NULL
+     */
+    private $prmDeclList;
+    
     /**
      * The ellipsis flag (default to FALSE).
      * @var bool
@@ -39,6 +46,36 @@ class ParameterDeclarationClause
     public function hasEllipsis(): bool
     {
         return $this->ellipsis;
+    }
+    
+    /**
+     * Sets the parameter declaration list.
+     * 
+     * @param   ParameterDeclarationList    $prmDeclList    The parameter declaration list to set.
+     */
+    public function setParameterDeclarationList(ParameterDeclarationList $prmDeclList): void
+    {
+        $this->prmDeclList = $prmDeclList;
+    }
+    
+    /**
+     * Returns the parameter declaration list.
+     * 
+     * @return  ParameterDeclarationList|NULL   The instance of the parameter declaration list if it has been set, otherwise NULL.
+     */
+    public function getParameterDeclarationList(): ?ParameterDeclarationList
+    {
+        return $this->prmDeclList;
+    }
+    
+    /**
+     * Indicates whether a parameter declaration list has been set.
+     * 
+     * @return  bool    TRUE if a parameter declaration list has been set, otherwise FALSE.
+     */
+    public function hasParameterDeclarationList(): bool
+    {
+        return $this->prmDeclList !== NULL;
     }
 }
 
--- test/PhpCode/Test/Unit/Language/Cpp/Declarator/ParameterDeclarationClauseTest.php
+++ test/PhpCode/Test/Unit/Language/Cpp/Declarator/ParameterDeclarationClauseTest.php
@@ -8,7 +8,9 @@
 namespace PhpCode\Test\Unit\Language\Cpp\Declarator;
 
 use PhpCode\Language\Cpp\Declarator\ParameterDeclarationClause;
+use PhpCode\Language\Cpp\Declarator\ParameterDeclarationList;
 use PHPUnit\Framework\TestCase;
+use Prophecy\Prophecy\ProphecySubjectInterface;
 
 /**
  * Represents the unit tests for the {@see PhpCode\Language\Cpp\Declarator\ParameterDeclarationClause} 
@@ -49,6 +51,55 @@ class ParameterDeclarationClauseTest extends TestCase
         
         $this->sut->addEllipsis();
         self::assertTrue($this->sut->hasEllipsis());
+    }
+    
+    /**
+     * Tests that getParameterDeclarationList() returns:
+     * - NULL when instantiated, and 
+     * - the instance of ParameterDeclarationList that has been set with 
+     *   setParameterDeclarationList().
+     */
+    public function testGetParameterDeclarationList(): void
+    {
+        self::assertNull($this->sut->getParameterDeclarationList());
+        
+        $prmDeclList1 = $this->createParameterDeclarationListDummy();
+        $this->sut->setParameterDeclarationList($prmDeclList1);
+        self::assertSame($prmDeclList1, $this->sut->getParameterDeclarationList());
+        
+        $prmDeclList2 = $this->createParameterDeclarationListDummy();
+        $this->sut->setParameterDeclarationList($prmDeclList2);
+        self::assertSame($prmDeclList2, $this->sut->getParameterDeclarationList());
+    }
+    
+    /**
+     * Tests that hasParameterDeclarationList() returns:
+     * - FALSE when instantiated, and 
+     * - TRUE when an instance of ParameterDeclarationList has been set with 
+     *   setParameterDeclarationList().
+     */
+    public function testHasParameterDeclarationListReturnsBool(): void
+    {
+        self::assertFalse($this->sut->hasParameterDeclarationList());
+        
+        $prmDeclList1 = $this->createParameterDeclarationListDummy();
+        $this->sut->setParameterDeclarationList($prmDeclList1);
+        self::assertTrue($this->sut->hasParameterDeclarationList());
+        
+        $prmDeclList2 = $this->createParameterDeclarationListDummy();
+        $this->sut->setParameterDeclarationList($prmDeclList2);
+        self::assertTrue($this->sut->hasParameterDeclarationList());
+    }
+    
+    /**
+     * Creates a dummy of the {@see PhpCode\Language\Cpp\Declarator\ParameterDeclarationList} 
+     * class.
+     * 
+     * @return  ProphecySubjectInterface
+     */
+    private function createParameterDeclarationListDummy(): ProphecySubjectInterface
+    {
+        return $this->prophesize(ParameterDeclarationList::class)->reveal();
     }
 }
 
