8851244ebe6ce8955c001888e5819d3d4e4ef353
Christophe Maymard <christophe.maymard@hotmail.com>
2020-03-07 11:51:29 +0100

    [C++] Create declaration specifier.

---
+++ src/PhpCode/Language/Cpp/Declaration/DeclarationSpecifier.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * This file is part of the PhpCode library.
+ * 
+ * @copyright   2020, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpCode\Language\Cpp\Declaration;
+
+/**
+ * Represents a declaration specifier.
+ * 
+ * decl-specifier:
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class DeclarationSpecifier
+{
+    /**
+     * Private constructor.
+     */
+    private function __construct()
+    {
+    }
+}
+
---
+++ test/PhpCode/Test/Unit/Language/Cpp/Declaration/DeclarationSpecifierTest.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * This file is part of the PhpCode library.
+ * 
+ * @copyright   2020, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpCode\Test\Unit\Language\Cpp\Declaration;
+
+use PhpCode\Language\Cpp\Declaration\DeclarationSpecifier;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Represents the unit tests for the {@see PhpCode\Language\Cpp\Declaration\DeclarationSpecifier} 
+ * class.
+ * 
+ * @group   declaration
+ * @group   c++
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class DeclarationSpecifierTest extends TestCase
+{
+    /**
+     * Tests that __construct() throws an exception when instantiated.
+     */
+    public function test__constructThrowsExceptionWhenInstantiated(): void
+    {
+        $this->expectException(\Throwable::class);
+        $this->expectExceptionMessageMatches('`private `');
+        $sut = new DeclarationSpecifier();
+    }
+}
+
