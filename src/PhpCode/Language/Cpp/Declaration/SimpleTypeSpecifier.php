9d8a178d37945796e06238e65f7f50484c5fdd0c
Christophe Maymard <christophe.maymard@hotmail.com>
2020-03-07 10:52:28 +0100

    [C++] Create simple type specifier.

---
+++ src/PhpCode/Language/Cpp/Declaration/SimpleTypeSpecifier.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * This file is part of the PhpCode library.
+ * 
+ * @copyright   2020, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpCode\Language\Cpp\Declaration;
+
+/**
+ * Represents a simple type specifier.
+ * 
+ * simple-type-specifier:
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class SimpleTypeSpecifier
+{
+    /**
+     * Private constructor.
+     */
+    private function __construct()
+    {
+    }
+}
+
