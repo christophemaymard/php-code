4dfe32a8a1a7741bd162e434eeef38bf8adf5de2
Christophe Maymard <christophe.maymard@hotmail.com>
2020-03-07 11:46:12 +0100

    [C++] Defining type specifier is defined as a type specifier.

--- src/PhpCode/Language/Cpp/Declaration/DefiningTypeSpecifier.php
+++ src/PhpCode/Language/Cpp/Declaration/DefiningTypeSpecifier.php
@@ -11,16 +11,48 @@ namespace PhpCode\Language\Cpp\Declaration;
  * Represents a defining type specifier.
  * 
  * defining-type-specifier:
+ *     type-specifier
  * 
  * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
  */
 class DefiningTypeSpecifier
 {
     /**
+     * The type specifier.
+     * @var TypeSpecifier
+     */
+    private $typeSpec;
+    
+    /**
+     * Creates an instance of a defining type specifier defined as a type 
+     * specifier.
+     * 
+     * @param   TypeSpecifier   $typeSpec   The type specifier.
+     * @return  DefiningTypeSpecifier   The created instance of DefiningTypeSpecifier.
+     */
+    public static function createTypeSpecifier(TypeSpecifier $typeSpec): self
+    {
+        $defSypeSpec = new self();
+        $defSypeSpec->typeSpec = $typeSpec;
+        
+        return $defSypeSpec;
+    }
+    
+    /**
      * Private constructor.
      */
     private function __construct()
     {
+    }
+    
+    /**
+     * Returns the type specifier.
+     * 
+     * @return  TypeSpecifier
+     */
+    public function getTypeSpecifier(): TypeSpecifier
+    {
+        return $this->typeSpec;
     }
 }
 
--- test/PhpCode/Test/Unit/Language/Cpp/Declaration/DefiningTypeSpecifierTest.php
+++ test/PhpCode/Test/Unit/Language/Cpp/Declaration/DefiningTypeSpecifierTest.php
@@ -8,6 +8,7 @@
 namespace PhpCode\Test\Unit\Language\Cpp\Declaration;
 
 use PhpCode\Language\Cpp\Declaration\DefiningTypeSpecifier;
+use PhpCode\Language\Cpp\Declaration\TypeSpecifier;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -29,6 +30,32 @@ class DefiningTypeSpecifierTest extends TestCase
         $this->expectException(\Throwable::class);
         $this->expectExceptionMessageMatches('`private `');
         $sut = new DefiningTypeSpecifier();
+    }
+    
+    /**
+     * Tests that createTypeSpecifier() returns new instances of 
+     * DefiningTypeSpecifier.
+     */
+    public function testCreateTypeSpecifierReturnsNewInstanceDefiningTypeSpecifier(): void
+    {
+        $typeSpec = $this->prophesize(TypeSpecifier::class)->reveal();
+        
+        $defSypeSpec1 = DefiningTypeSpecifier::createTypeSpecifier($typeSpec);
+        $defSypeSpec2 = DefiningTypeSpecifier::createTypeSpecifier($typeSpec);
+        self::assertNotSame($defSypeSpec1, $defSypeSpec2);
+    }
+    
+    /**
+     * Tests that getTypeSpecifier() returns the instance of 
+     * TypeSpecifier when the instance has been created by 
+     * createTypeSpecifier().
+     */
+    public function testGetTypeSpecifierReturnsTypeSpecifierWhenCreateTypeSpecifier(): void
+    {
+        $typeSpec = $this->prophesize(TypeSpecifier::class)->reveal();
+        
+        $sut = DefiningTypeSpecifier::createTypeSpecifier($typeSpec);
+        self::assertSame($typeSpec, $sut->getTypeSpecifier());
     }
 }
 
