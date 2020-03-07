072f5ce5ff7e6bbfb8d0a9e29fb03fc87de45b83
Christophe Maymard <christophe.maymard@hotmail.com>
2020-03-07 11:41:29 +0100

    [C++] Create defining type specifier.

---
+++ src/PhpCode/Language/Cpp/Declaration/DefiningTypeSpecifier.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * This file is part of the PhpCode library.
+ * 
+ * @copyright   2020, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpCode\Language\Cpp\Declaration;
+
+/**
+ * Represents a defining type specifier.
+ * 
+ * defining-type-specifier:
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class DefiningTypeSpecifier
+{
+    /**
+     * Private constructor.
+     */
+    private function __construct()
+    {
+    }
+}
+
