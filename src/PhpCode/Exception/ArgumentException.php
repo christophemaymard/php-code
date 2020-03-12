7ecc5ac0a18b9de7c46f626d6fcfa4d8e54f0048
Christophe Maymard <christophe.maymard@hotmail.com>
2020-03-12 15:22:44 +0100

    Create ArgumentException class.

---
+++ src/PhpCode/Exception/ArgumentException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of the PhpCode library.
+ * 
+ * @copyright   2020, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpCode\Exception;
+
+/**
+ * Represents the exception thrown when an argument is invalid.
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class ArgumentException extends \Exception implements PhpCodeExceptionInterface
+{
+}
+
