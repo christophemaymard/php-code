93431ce189266bf46c9caf4d5803687e55d4213d
Christophe Maymard <christophe.maymard@hotmail.com>
2020-03-14 23:59:21 +0100

    [C++] Add DeclaratorIdDoubleFactory::createGetIdExpression().

--- test/PhpCode/Test/Language/Cpp/Declarator/DeclaratorIdDoubleFactory.php
+++ test/PhpCode/Test/Language/Cpp/Declarator/DeclaratorIdDoubleFactory.php
@@ -8,7 +8,9 @@
 namespace PhpCode\Test\Language\Cpp\Declarator;
 
 use PhpCode\Language\Cpp\Declarator\DeclaratorId;
+use PhpCode\Language\Cpp\Expression\IdExpression;
 use PhpCode\Test\AbstractDoubleFactory;
+use Prophecy\Prophecy\ProphecySubjectInterface;
 
 /**
  * Represents a factory of doubles for the {@see PhpCode\Language\Cpp\Declarator\DeclaratorId} 
@@ -24,6 +26,22 @@ class DeclaratorIdDoubleFactory extends AbstractDoubleFactory
     protected function getClassName(): string
     {
         return DeclaratorId::class;
+    }
+    
+    /**
+     * Creates a double where getIdExpression() can be called.
+     * 
+     * @param   IdExpression    $return The value to return when getIdExpression() is called.
+     * @return  ProphecySubjectInterface
+     */
+    public function createGetIdExpression(IdExpression $return): ProphecySubjectInterface
+    {
+        $prophecy = $this->prophesize();
+        $prophecy
+            ->getIdExpression()
+            ->willReturn($return);
+        
+        return $prophecy->reveal();
     }
 }
 
