f8e7818af459015d31ad71960a4e47dd09beb8a8
Christophe Maymard <christophe.maymard@hotmail.com>
2020-03-14 14:19:21 +0100

    [C++] Create factory of parameter declaration clause constraint doubles.

---
+++ test/PhpCode/Test/Language/Cpp/Declarator/ParameterDeclarationClauseConstraintDoubleFactory.php
@@ -0,0 +1,155 @@
+<?php
+/**
+ * This file is part of the PhpCode library.
+ * 
+ * @copyright   2020, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpCode\Test\Language\Cpp\Declarator;
+
+use PhpCode\Language\Cpp\Declarator\ParameterDeclarationClause;
+use PhpCode\Test\Language\Cpp\AbstractConceptConstraintDoubleFactory;
+use Prophecy\Prophecy\ObjectProphecy;
+use Prophecy\Prophecy\ProphecySubjectInterface;
+
+/**
+ * Represents a factory of doubles for the {@see PhpCode\Test\Language\Cpp\Declarator\ParameterDeclarationClauseConstraint} 
+ * class.
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class ParameterDeclarationClauseConstraintDoubleFactory extends AbstractConceptConstraintDoubleFactory
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function getClassName(): string
+    {
+        return ParameterDeclarationClauseConstraint::class;
+    }
+    
+    /**
+     * Creates a double where matches() can be called.
+     * 
+     * @param   ParameterDeclarationClause  $prmDeclClause  The value of the first argument when matches() is called.
+     * @param   bool                        $return         The value to return when matches() is called.
+     * @return  ProphecySubjectInterface
+     */
+    public function createMatches(
+        ParameterDeclarationClause $prmDeclClause, 
+        bool $return
+    ): ProphecySubjectInterface
+    {
+        $prophecy = $this->prophesize();
+        
+        $this->buildMatches($prophecy, $prmDeclClause, $return);
+        
+        return $prophecy->reveal();
+    }
+    
+    /**
+     * Creates a double where matches() and failureReason() can be called.
+     * 
+     * @param   ParameterDeclarationClause  $prmDeclClause  The value of the first argument when matches() or failureReason() is called.
+     * @param   bool                        $matches        The value to return when matches() is called.
+     * @param   string                      $failureReason  The value to return when failureReason() is called.
+     * @return  ProphecySubjectInterface
+     */
+    public function createMatchesFailureReason(
+        ParameterDeclarationClause $prmDeclClause, 
+        bool $matches, 
+        string $failureReason
+    ): ProphecySubjectInterface
+    {
+        $prophecy = $this->prophesize();
+        $this->buildMatches($prophecy, $prmDeclClause, $matches);
+        $this->buildFailureReason($prophecy, $prmDeclClause, $failureReason);
+        
+        return $prophecy->reveal();
+    }
+    
+    /**
+     * Creates a double where matches() and constraintDescription() can be 
+     * called.
+     * 
+     * @param   ParameterDeclarationClause  $prmDeclClause  The value of the first argument when matches() is called.
+     * @param   bool                        $matches        The value to return when matches() is called.
+     * @param   string                      $constDesc      The value to return when constraintDescription() is called.
+     * @return  ProphecySubjectInterface
+     */
+    public function createMatchesConstraintDescription(
+        ParameterDeclarationClause $prmDeclClause, 
+        bool $matches, 
+        string $constDesc
+    ): ProphecySubjectInterface
+    {
+        $prophecy = $this->prophesize();
+        $this->buildMatches($prophecy, $prmDeclClause, $matches);
+        $this->buildConstraintDescription($prophecy, $constDesc);
+        
+        return $prophecy->reveal();
+    }
+    
+    /**
+     * Creates a double where matches(), failureReason() and 
+     * constraintDescription() can be called.
+     * 
+     * @param   ParameterDeclarationClause  $prmDeclClause  The value of the first argument when matches() or failureReason() is called.
+     * @param   bool                        $matches        The value to return when matches() is called.
+     * @param   string                      $failureReason  The value to return when failureReason() is called.
+     * @param   string                      $constDesc      The value to return when constraintDescription() is called.
+     * @return  ProphecySubjectInterface
+     */
+    public function createMatchesFailureReasonConstraintDescription(
+        ParameterDeclarationClause $prmDeclClause, 
+        bool $matches, 
+        string $failureReason, 
+        string $constDesc
+    ): ProphecySubjectInterface
+    {
+        $prophecy = $this->prophesize();
+        $this->buildMatches($prophecy, $prmDeclClause, $matches);
+        $this->buildFailureReason($prophecy, $prmDeclClause, $failureReason);
+        $this->buildConstraintDescription($prophecy, $constDesc);
+        
+        return $prophecy->reveal();
+    }
+    
+    /**
+     * Builds and adds a prophecy of matches() to the specified prophecy.
+     * 
+     * @param   ObjectProphecy              $prophecy       The prophecy to build to.
+     * @param   ParameterDeclarationClause  $prmDeclClause  The value of the first argument when matches() is called.
+     * @param   bool                        $return         The value to return when matches() is called.
+     */
+    private function buildMatches(
+        ObjectProphecy $prophecy, 
+        ParameterDeclarationClause $prmDeclClause, 
+        bool $return
+    ): void
+    {
+        $prophecy
+            ->matches($prmDeclClause)
+            ->willReturn($return);
+    }
+    
+    /**
+     * Builds and adds a prophecy of failureReason() to the specified 
+     * prophecy.
+     * 
+     * @param   ObjectProphecy              $prophecy       The prophecy to build to.
+     * @param   ParameterDeclarationClause  $prmDeclClause  The value of the first argument when failureReason() is called.
+     * @param   string                      $return         The value to return when failureReason() is called.
+     */
+    private function buildFailureReason(
+        ObjectProphecy $prophecy, 
+        ParameterDeclarationClause $prmDeclClause, 
+        string $return
+    ): void
+    {
+        $prophecy
+            ->failureReason($prmDeclClause)
+            ->willReturn($return);
+    }
+}
+
