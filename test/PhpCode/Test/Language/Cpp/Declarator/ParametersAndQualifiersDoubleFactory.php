976f8f2b439adca9849afd57f430d492e3265a49
Christophe Maymard <christophe.maymard@hotmail.com>
2020-03-14 14:18:29 +0100

    [C++] Add ParametersAndQualifiersDoubleFactory::createGetParameterDeclarationClause().

--- test/PhpCode/Test/Language/Cpp/Declarator/ParametersAndQualifiersDoubleFactory.php
+++ test/PhpCode/Test/Language/Cpp/Declarator/ParametersAndQualifiersDoubleFactory.php
@@ -7,8 +7,10 @@
  */
 namespace PhpCode\Test\Language\Cpp\Declarator;
 
+use PhpCode\Language\Cpp\Declarator\ParameterDeclarationClause;
 use PhpCode\Language\Cpp\Declarator\ParametersAndQualifiers;
 use PhpCode\Test\AbstractDoubleFactory;
+use Prophecy\Prophecy\ProphecySubjectInterface;
 
 /**
  * Represents a factory of doubles for the {@see PhpCode\Language\Cpp\Declarator\ParametersAndQualifiers} 
@@ -24,6 +26,24 @@ class ParametersAndQualifiersDoubleFactory extends AbstractDoubleFactory
     protected function getClassName(): string
     {
         return ParametersAndQualifiers::class;
+    }
+    
+    /**
+     * Creates a double where getParameterDeclarationClause() can be called.
+     * 
+     * @param   ParameterDeclarationClause  $return The value to return when getParameterDeclarationClause() is called.
+     * @return  ProphecySubjectInterface
+     */
+    public function createGetParameterDeclarationClause(
+        ParameterDeclarationClause $return
+    ): ProphecySubjectInterface
+    {
+        $prophecy = $this->prophesize();
+        $prophecy
+            ->getParameterDeclarationClause()
+            ->willReturn($return);
+        
+        return $prophecy->reveal();
     }
 }
 
