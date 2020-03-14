e36164eab8da044635aae7f52e47d50a496f0083
Christophe Maymard <christophe.maymard@hotmail.com>
2020-03-14 12:05:45 +0100

    [C++] Update factory of parameter declaration clause doubles.
    - Added ParameterDeclarationClauseDoubleFactory::createGetParameterDeclarationList().
    - Added ParameterDeclarationClauseDoubleFactory::createHasEllipsisGetParameterDeclarationList().

--- test/PhpCode/Test/Language/Cpp/Declarator/ParameterDeclarationClauseDoubleFactory.php
+++ test/PhpCode/Test/Language/Cpp/Declarator/ParameterDeclarationClauseDoubleFactory.php
@@ -8,7 +8,9 @@
 namespace PhpCode\Test\Language\Cpp\Declarator;
 
 use PhpCode\Language\Cpp\Declarator\ParameterDeclarationClause;
+use PhpCode\Language\Cpp\Declarator\ParameterDeclarationList;
 use PhpCode\Test\AbstractDoubleFactory;
+use Prophecy\Prophecy\ProphecySubjectInterface;
 
 /**
  * Represents a factory of doubles for the {@see PhpCode\Language\Cpp\Declarator\ParameterDeclarationClause} 
@@ -24,6 +26,49 @@ class ParameterDeclarationClauseDoubleFactory extends AbstractDoubleFactory
     protected function getClassName(): string
     {
         return ParameterDeclarationClause::class;
+    }
+    
+    /**
+     * Creates a double where getParameterDeclarationList() can be called.
+     * 
+     * @param   ParameterDeclarationList    $return The value to return when getParameterDeclarationList() is called.
+     * @return  ProphecySubjectInterface
+     */
+    public function createGetParameterDeclarationList(
+        ParameterDeclarationList $return
+    ): ProphecySubjectInterface
+    {
+        $prophecy = $this->prophesize();
+        $prophecy
+            ->getParameterDeclarationList()
+            ->willReturn($return);
+        
+        return $prophecy->reveal();
+    }
+    
+    /**
+     * Creates a double where getParameterDeclarationList() and hasEllipsis() 
+     * can be called.
+     * 
+     * @param   bool                        $hasEllipsis    The value to return when hasEllipsis() is called.
+     * @param   ParameterDeclarationList    $getPrmDeclList The value to return when getParameterDeclarationList() is called (optional)(default to NULL).
+     * @return  ProphecySubjectInterface
+     */
+    public function createHasEllipsisGetParameterDeclarationList(
+        bool $hasEllipsis, 
+        ParameterDeclarationList $getPrmDeclList = NULL
+    ): ProphecySubjectInterface
+    {
+        $prophecy = $this->prophesize();
+        $prophecy
+            ->getParameterDeclarationList()
+            ->willReturn($getPrmDeclList);
+        
+        $prophecy
+            ->hasEllipsis()
+            ->willReturn($hasEllipsis);
+        
+        return $prophecy->reveal();
     }
 }
 
