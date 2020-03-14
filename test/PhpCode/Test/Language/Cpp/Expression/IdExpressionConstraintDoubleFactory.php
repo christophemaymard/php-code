a521eae4cc3d440e27036d13951d5cd45b55fd96
Christophe Maymard <christophe.maymard@hotmail.com>
2020-03-15 00:01:10 +0100

    [C++] Create factory of identifier expression constraint doubles.

---
+++ test/PhpCode/Test/Language/Cpp/Expression/IdExpressionConstraintDoubleFactory.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * This file is part of the PhpCode library.
+ * 
+ * @copyright   2020, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpCode\Test\Language\Cpp\Expression;
+
+use PhpCode\Language\Cpp\Expression\IdExpression;
+use PhpCode\Test\Language\Cpp\AbstractConceptConstraintDoubleFactory;
+use Prophecy\Prophecy\ProphecySubjectInterface;
+
+/**
+ * Represents a factory of doubles for the {@see PhpCode\Test\Language\Cpp\Expression\IdExpressionConstraint} 
+ * class.
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class IdExpressionConstraintDoubleFactory extends AbstractConceptConstraintDoubleFactory
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function getClassName(): string
+    {
+        return IdExpressionConstraint::class;
+    }
+    
+    /**
+     * Creates a double where matches() can be called.
+     * 
+     * @param   IdExpression    $idExpr The value of the first argument when matches() is called.
+     * @param   bool            $return The value to return when matches() is called.
+     * @return  ProphecySubjectInterface
+     */
+    public function createMatches(IdExpression $idExpr, bool $return): ProphecySubjectInterface
+    {
+        $prophecy = $this->prophesize();
+        
+        $this->buildMatches($prophecy, $idExpr, $return);
+        
+        return $prophecy->reveal();
+    }
+    
+    /**
+     * Creates a double where matches() and failureReason() can be called.
+     * 
+     * @param   IdExpression    $idExpr         The value of the first argument when matches() or failureReason() is called.
+     * @param   bool            $matches        The value to return when matches() is called.
+     * @param   string          $failureReason  The value to return when failureReason() is called.
+     * @return  ProphecySubjectInterface
+     */
+    public function createMatchesFailureReason(
+        IdExpression $idExpr, 
+        bool $matches, 
+        string $failureReason
+    ): ProphecySubjectInterface
+    {
+        $prophecy = $this->prophesize();
+        $this->buildMatches($prophecy, $idExpr, $matches);
+        $this->buildFailureReason($prophecy, $idExpr, $failureReason);
+        
+        return $prophecy->reveal();
+    }
+    
+    /**
+     * Creates a double where matches() and constraintDescription() can be 
+     * called.
+     * 
+     * @param   IdExpression    $idExpr     The value of the first argument when matches() is called.
+     * @param   bool            $matches    The value to return when matches() is called.
+     * @param   string          $constDesc  The value to return when constraintDescription() is called.
+     * @return  ProphecySubjectInterface
+     */
+    public function createMatchesConstraintDescription(
+        IdExpression $idExpr, 
+        bool $matches, 
+        string $constDesc
+    ): ProphecySubjectInterface
+    {
+        $prophecy = $this->prophesize();
+        $this->buildMatches($prophecy, $idExpr, $matches);
+        $this->buildConstraintDescription($prophecy, $constDesc);
+        
+        return $prophecy->reveal();
+    }
+    
+    /**
+     * Creates a double where matches(), failureReason() and 
+     * constraintDescription() can be called.
+     * 
+     * @param   IdExpression    $idExpr         The value of the first argument when matches() or failureReason() is called.
+     * @param   bool            $matches        The value to return when matches() is called.
+     * @param   string          $failureReason  The value to return when failureReason() is called.
+     * @param   string          $constDesc      The value to return when constraintDescription() is called.
+     * @return  ProphecySubjectInterface
+     */
+    public function createMatchesFailureReasonConstraintDescription(
+        IdExpression $idExpr, 
+        bool $matches, 
+        string $failureReason, 
+        string $constDesc
+    ): ProphecySubjectInterface
+    {
+        $prophecy = $this->prophesize();
+        $this->buildMatches($prophecy, $idExpr, $matches);
+        $this->buildFailureReason($prophecy, $idExpr, $failureReason);
+        $this->buildConstraintDescription($prophecy, $constDesc);
+        
+        return $prophecy->reveal();
+    }
+}
+
