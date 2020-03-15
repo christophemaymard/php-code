3976aed04c721c41b23f1ec2bd9425c029e59678
Christophe Maymard <christophe.maymard@hotmail.com>
2020-03-15 13:45:26 +0100

    [C++] Create a builder of declarator identifier double.

---
+++ test/PhpCode/Test/Language/Cpp/Declarator/DeclaratorIdDoubleBuilder.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * This file is part of the PhpCode library.
+ * 
+ * @copyright   2020, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpCode\Test\Language\Cpp\Declarator;
+
+use PhpCode\Language\Cpp\Declarator\DeclaratorId;
+use PhpCode\Language\Cpp\Expression\IdExpression;
+use PhpCode\Test\AbstractDoubleBuilder;
+
+/**
+ * Represents a double builder of the {@see PhpCode\Language\Cpp\Declarator\DeclaratorId} 
+ * class.
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class DeclaratorIdDoubleBuilder extends AbstractDoubleBuilder
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function getClassInterfaceName(): string
+    {
+        return DeclaratorId::class;
+    }
+    
+    /**
+     * Builds and adds a prophecy where getIdExpression() can be called.
+     * 
+     * @param   IdExpression    $return The value to return when getIdExpression() is called.
+     * @return  DeclaratorIdDoubleBuilder   This instance.
+     */
+    public function buildGetIdExpression(IdExpression $return): self
+    {
+        $this->getProphecy()
+            ->getIdExpression()
+            ->willReturn($return);
+        
+        return $this;
+    }
+}
+
