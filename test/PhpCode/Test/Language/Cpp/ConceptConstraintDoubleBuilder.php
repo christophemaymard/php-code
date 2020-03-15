dfff2a3f2c6d62cf6371a3d6ba3fa17acb950ff1
Christophe Maymard <christophe.maymard@hotmail.com>
2020-03-15 17:24:43 +0100

    [C++] Create a factory of concept constraint double builders.

---
+++ test/PhpCode/Test/Language/Cpp/ConceptConstraintDoubleBuilder.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * This file is part of the PhpCode library.
+ * 
+ * @copyright   2020, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpCode\Test\Language\Cpp;
+
+use PhpCode\Test\Language\Cpp\Declarator\DeclaratorIdConstraintDoubleBuilder;
+use PhpCode\Test\Language\Cpp\Declarator\ParametersAndQualifiersConstraintDoubleBuilder;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Represents a factory of concept constraint double builders.
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class ConceptConstraintDoubleBuilder
+{
+    /**
+     * Creates a double constraint builder of the {@see PhpCode\Test\Language\Cpp\Declarator\DeclaratorIdConstraint} 
+     * class.
+     * 
+     * @param   TestCase    $testCase   The test case used to prophesize a class or an interface.
+     * @return  DeclaratorIdConstraintDoubleBuilder The created instance of DeclaratorIdConstraintDoubleBuilder.
+     */
+    public static function createDeclaratorIdConstraint(TestCase $testCase): DeclaratorIdConstraintDoubleBuilder
+    {
+        return new DeclaratorIdConstraintDoubleBuilder($testCase);
+    }
+    
+    /**
+     * Creates a double constraint builder of the {@see PhpCode\Test\Language\Cpp\Declarator\ParametersAndQualifiersConstraint} 
+     * class.
+     * 
+     * @param   TestCase    $testCase   The test case used to prophesize a class or an interface.
+     * @return  ParametersAndQualifiersConstraintDoubleBuilder  The created instance of ParametersAndQualifiersConstraintDoubleBuilder.
+     */
+    public static function createParametersAndQualifiersConstraint(TestCase $testCase): ParametersAndQualifiersConstraintDoubleBuilder
+    {
+        return new ParametersAndQualifiersConstraintDoubleBuilder($testCase);
+    }
+}
+
