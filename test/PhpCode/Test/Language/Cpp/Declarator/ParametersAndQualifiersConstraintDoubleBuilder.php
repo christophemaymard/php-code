ece744a91ee7bac84029a81754d1c33cc4afb652
Christophe Maymard <christophe.maymard@hotmail.com>
2020-03-15 17:23:32 +0100

    [C++] Create a builder of parameters and qualifiers constraint double.

---
+++ test/PhpCode/Test/Language/Cpp/Declarator/ParametersAndQualifiersConstraintDoubleBuilder.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This file is part of the PhpCode library.
+ * 
+ * @copyright   2020, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpCode\Test\Language\Cpp\Declarator;
+
+use PhpCode\Test\Language\Cpp\AbstractConceptConstraintDoubleBuilder;
+
+/**
+ * Represents a double constraint builder of the {@see PhpCode\Test\Language\Cpp\Declarator\ParametersAndQualifiersConstraint} 
+ * class.
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class ParametersAndQualifiersConstraintDoubleBuilder extends AbstractConceptConstraintDoubleBuilder
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function getClassInterfaceName(): string
+    {
+        return ParametersAndQualifiersConstraint::class;
+    }
+}
+
