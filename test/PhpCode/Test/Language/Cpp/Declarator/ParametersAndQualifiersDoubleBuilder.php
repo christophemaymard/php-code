0bb1be09863817d6108a58ca3f8d8058946dfe33
Christophe Maymard <christophe.maymard@hotmail.com>
2020-03-15 13:28:21 +0100

    [C++] Create a builder of parameters and qualifiers double.

---
+++ test/PhpCode/Test/Language/Cpp/Declarator/ParametersAndQualifiersDoubleBuilder.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * This file is part of the PhpCode library.
+ * 
+ * @copyright   2020, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpCode\Test\Language\Cpp\Declarator;
+
+use PhpCode\Language\Cpp\Declarator\ParameterDeclarationClause;
+use PhpCode\Language\Cpp\Declarator\ParametersAndQualifiers;
+use PhpCode\Test\AbstractDoubleBuilder;
+
+/**
+ * Represents a double builder of the {@see PhpCode\Language\Cpp\Declarator\ParametersAndQualifiers} 
+ * class.
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class ParametersAndQualifiersDoubleBuilder extends AbstractDoubleBuilder
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function getClassInterfaceName(): string
+    {
+        return ParametersAndQualifiers::class;
+    }
+    
+    /**
+     * Builds and adds a prophecy where getParameterDeclarationClause() can 
+     * be called.
+     * 
+     * @param   ParameterDeclarationClause  $return The value to return when getParameterDeclarationClause() is called.
+     * @return  ParametersAndQualifiersDoubleBuilder    This instance.
+     */
+    public function buildGetParameterDeclarationClause(
+        ParameterDeclarationClause $return
+    ): self
+    {
+        $this->getProphecy()
+            ->getParameterDeclarationClause()
+            ->willReturn($return);
+        
+        return $this;
+    }
+}
+
