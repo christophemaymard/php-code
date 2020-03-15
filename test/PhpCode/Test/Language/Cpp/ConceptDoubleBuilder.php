1a57da7fb8ac73a7dca7b430e9726810e0500817
Christophe Maymard <christophe.maymard@hotmail.com>
2020-03-15 13:28:49 +0100

    [C++] Create a factory of concept double builders.

---
+++ test/PhpCode/Test/Language/Cpp/ConceptDoubleBuilder.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * This file is part of the PhpCode library.
+ * 
+ * @copyright   2020, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpCode\Test\Language\Cpp;
+
+use PhpCode\Test\Language\Cpp\Declarator\ParametersAndQualifiersDoubleBuilder;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Represents a factory of concept double builders.
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class ConceptDoubleBuilder
+{
+    /**
+     * Creates a double builder of the {@see PhpCode\Language\Cpp\Declarator\ParametersAndQualifiers} 
+     * class.
+     * 
+     * @param   TestCase    $testCase   The test case used to prophesize a class or an interface.
+     * @return  ParametersAndQualifiersDoubleBuilder    The created instance of ParametersAndQualifiersDoubleBuilder.
+     */
+    public static function createParametersAndQualifiers(TestCase $testCase): ParametersAndQualifiersDoubleBuilder
+    {
+        return new ParametersAndQualifiersDoubleBuilder($testCase);
+    }
+}
+
