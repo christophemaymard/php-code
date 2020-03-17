fa8158d412cf05f2a7611f92573d99bbd9d7edb2
Christophe Maymard <christophe.maymard@hotmail.com>
2020-03-17 21:35:06 +0100

    [C++] Create provider of parameter declaration lists.

---
+++ test/PhpCode/Test/Language/Cpp/Parsing/ParameterDeclarationListProvider.php
@@ -0,0 +1,189 @@
+<?php
+/**
+ * This file is part of the PhpCode library.
+ * 
+ * @copyright   2020, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpCode\Test\Language\Cpp\Parsing;
+
+use PhpCode\Test\Language\Cpp\CallableConceptConstraintFactory;
+use PhpCode\Test\Language\Cpp\Declarator\ParameterDeclarationListConstraint;
+
+/**
+ * Represents the data provider related to parameter declaration lists.
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class ParameterDeclarationListProvider
+{
+    /**
+     * Returns a set of valid data for the provider.
+     * 
+     * @return  ValidData[]
+     */
+    public static function createValidDataSetProvider(): array
+    {
+        $dataSet = [];
+        
+        foreach (ParameterDeclarationProvider::createValidDataSet() as $data) {
+            $dataSet[] = self::createPrmDeclValidData($data);
+            $dataSet[] = self::create3PrmDeclValidData($data);
+            
+            $dataSet[] = self::create3PrmDeclEllipsisValidData($data);
+            $dataSet[] = self::create3PrmDeclWSEllipsisValidData($data);
+        }
+        
+        return $dataSet;
+    }
+    
+    /**
+     * Returns a set of valid data.
+     * 
+     * @return  ValidData[]
+     */
+    public static function createValidDataSet(): array
+    {
+        $dataSet = [];
+        
+        foreach (ParameterDeclarationProvider::createValidDataSet() as $data) {
+            $dataSet[] = self::createPrmDeclValidData($data);
+            $dataSet[] = self::create3PrmDeclValidData($data);
+        }
+        
+        return $dataSet;
+    }
+    
+    /**
+     * Creates a valid data for the case:
+     * PRM_DECL
+     * 
+     * @param   ValidData   $prmDeclData    The parameter declaration data used to create the data.
+     * @return  ValidData   The created instance of ValidData.
+     */
+    private static function createPrmDeclValidData(ValidData $prmDeclData): ValidData
+    {
+        $stream = $prmDeclData->getStream();
+        
+        $prmDeclFactory = $prmDeclData->getConstraintFactory();
+        $callable = function () use ($prmDeclFactory) {
+            return new ParameterDeclarationListConstraint([
+                $prmDeclFactory->createConstraint()
+            ]);
+        };
+        $factory = new CallableConceptConstraintFactory($callable);
+        
+        $data = new ValidData($stream, $factory);
+        
+        $data->setName($prmDeclData->getName());
+        
+        return $data;
+    }
+    
+    /**
+     * Creates a valid data for the case:
+     * PRM_DECL COMMA PRM_DECL COMMA PRM_DECL
+     * 
+     * @param   ValidData   $prmDeclData    The parameter declaration data used to create the data.
+     * @return  ValidData   The created instance of ValidData.
+     */
+    private static function create3PrmDeclValidData(ValidData $prmDeclData): ValidData
+    {
+        $prmDeclStream = $prmDeclData->getStream();
+        
+        $stream = \sprintf('%s,%s,%s', 
+            $prmDeclStream, 
+            $prmDeclStream, 
+            $prmDeclStream
+        );
+        
+        $prmDeclFactory = $prmDeclData->getConstraintFactory();
+        $callable = function () use ($prmDeclFactory) {
+            return new ParameterDeclarationListConstraint([
+                $prmDeclFactory->createConstraint(), 
+                $prmDeclFactory->createConstraint(), 
+                $prmDeclFactory->createConstraint(), 
+            ]);
+        };
+        $factory = new CallableConceptConstraintFactory($callable);
+        
+        $data = new ValidData($stream, $factory);
+        
+        $prmDeclName = $prmDeclData->getName();
+        $data->setName(\sprintf(
+            '%s, %s, %s', 
+            $prmDeclName, 
+            $prmDeclName, 
+            $prmDeclName, 
+        ));
+        
+        return $data;
+    }
+    
+    /**
+     * Creates a valid data for the case:
+     * PRM_DECL COMMA PRM_DECL COMMA PRM_DECL COMMA ELLIPSIS
+     * 
+     * @param   ValidData   $prmDeclData    The parameter declaration data used to create the data.
+     * @return  ValidData   The created instance of ValidData.
+     */
+    private static function create3PrmDeclEllipsisValidData(ValidData $prmDeclData): ValidData
+    {
+        $prmDeclStream = $prmDeclData->getStream();
+        $stream = \sprintf('%s,%s,%s,...', 
+            $prmDeclStream, 
+            $prmDeclStream, 
+            $prmDeclStream
+        );
+        
+        $prmDeclFactory = $prmDeclData->getConstraintFactory();
+        $callable = function () use ($prmDeclFactory) {
+            return new ParameterDeclarationListConstraint([
+                $prmDeclFactory->createConstraint(), 
+                $prmDeclFactory->createConstraint(), 
+                $prmDeclFactory->createConstraint(), 
+            ]);
+        };
+        $factory = new CallableConceptConstraintFactory($callable);
+        
+        $data = new ValidData($stream, $factory);
+        
+        $data->setToken(',', 249000);
+        
+        return $data;
+    }
+    
+    /**
+     * Creates a valid data for the case:
+     * PRM_DECL COMMA PRM_DECL COMMA PRM_DECL WS ELLIPSIS
+     * 
+     * @param   ValidData   $prmDeclData    The parameter declaration data used to create the data.
+     * @return  ValidData   The created instance of ValidData.
+     */
+    private static function create3PrmDeclWSEllipsisValidData(ValidData $prmDeclData): ValidData
+    {
+        $prmDeclStream = $prmDeclData->getStream();
+        $stream = \sprintf('%s,%s,%s ...', 
+            $prmDeclStream, 
+            $prmDeclStream, 
+            $prmDeclStream
+        );
+        
+        $prmDeclFactory = $prmDeclData->getConstraintFactory();
+        $callable = function () use ($prmDeclFactory) {
+            return new ParameterDeclarationListConstraint([
+                $prmDeclFactory->createConstraint(), 
+                $prmDeclFactory->createConstraint(), 
+                $prmDeclFactory->createConstraint(), 
+            ]);
+        };
+        $factory = new CallableConceptConstraintFactory($callable);
+        
+        $data = new ValidData($stream, $factory);
+        
+        $data->setToken('...', 211000);
+        
+        return $data;
+    }
+}
+
---
+++ test/PhpCode/Test/Unit/Test/Language/Cpp/Parsing/ParameterDeclarationListProviderTest.php
@@ -0,0 +1,122 @@
+<?php
+/**
+ * This file is part of the PhpCode library.
+ * 
+ * @copyright   2020, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpCode\Test\Unit\Test\Language\Cpp\Parsing;
+
+use PhpCode\Test\Language\Cpp\Declarator\ParameterDeclarationListConstraint;
+use PhpCode\Test\Language\Cpp\Parsing\ParameterDeclarationListProvider;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Represents the unit tests for the {@see PhpCode\Test\Language\Cpp\Parsing\ParameterDeclarationListProvider} 
+ * class.
+ * 
+ * @group   declarator
+ * @group   parsing
+ * @group   c++
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class ParameterDeclarationListProviderTest extends TestCase
+{
+    /**
+     * Tests createValidDataSetProvider() returns an array with at least one 
+     * valid data.
+     */
+    public function testCreateValidDataSetProviderReturnsAtLeastOneValidData(): void
+    {
+        $dataSet = ParameterDeclarationListProvider::createValidDataSetProvider();
+        self::assertTrue(\count($dataSet) > 0);
+    }
+    
+    /**
+     * Tests that createValidDataSetProvider() returns an array of valid data 
+     * that have a constraint factory that creates new instances.
+     */
+    public function testCreateValidDataSetProviderReturnsValidDataWithFactoryCreatesNewInstances(): void
+    {
+        $dataSet = ParameterDeclarationListProvider::createValidDataSetProvider();
+        
+        foreach ($dataSet as $data) {
+            $factory = $data->getConstraintFactory();
+            $const1 = $factory->createConstraint();
+            $const2 = $factory->createConstraint();
+            self::assertNotSame($const1, $const2);
+        }
+    }
+    
+    /**
+     * Tests that createValidDataSetProvider() returns an array of valid data 
+     * that have a constraint factory that creates instance of 
+     * ParameterDeclarationListConstraint.
+     */
+    public function testCreateValidDataSetProviderReturnsValidDataWithFactoryCreatesInstanceParameterDeclarationListConstraint(): void
+    {
+        $dataSet = ParameterDeclarationListProvider::createValidDataSetProvider();
+        
+        foreach ($dataSet as $data) {
+            $factory = $data->getConstraintFactory();
+            self::assertInstanceOf(
+                ParameterDeclarationListConstraint::class, 
+                $factory->createConstraint()
+            );
+            self::assertInstanceOf(
+                ParameterDeclarationListConstraint::class, 
+                $factory->createConstraint()
+            );
+        }
+    }
+    
+    /**
+     * Tests createValidDataSet() returns an array with at least one valid 
+     * data.
+     */
+    public function testCreateValidDataSetReturnsAtLeastOneValidData(): void
+    {
+        $dataSet = ParameterDeclarationListProvider::createValidDataSet();
+        self::assertTrue(\count($dataSet) > 0);
+    }
+    
+    /**
+     * Tests that createValidDataSet() returns an array of valid data that 
+     * have a constraint factory that creates new instances.
+     */
+    public function testCreateValidDataSetReturnsValidDataWithFactoryCreatesNewInstances(): void
+    {
+        $dataSet = ParameterDeclarationListProvider::createValidDataSet();
+        
+        foreach ($dataSet as $data) {
+            $factory = $data->getConstraintFactory();
+            $const1 = $factory->createConstraint();
+            $const2 = $factory->createConstraint();
+            self::assertNotSame($const1, $const2);
+        }
+    }
+    
+    /**
+     * Tests that createValidDataSet() returns an array of valid data that 
+     * have a constraint factory that creates instance of 
+     * ParameterDeclarationListConstraint.
+     */
+    public function testCreateValidDataSetReturnsValidDataWithFactoryCreatesInstanceParameterDeclarationListConstraint(): void
+    {
+        $dataSet = ParameterDeclarationListProvider::createValidDataSet();
+        
+        foreach ($dataSet as $data) {
+            $factory = $data->getConstraintFactory();
+            self::assertInstanceOf(
+                ParameterDeclarationListConstraint::class, 
+                $factory->createConstraint()
+            );
+            self::assertInstanceOf(
+                ParameterDeclarationListConstraint::class, 
+                $factory->createConstraint()
+            );
+        }
+    }
+}
+
