882565e5abdd08ca69155ef21b94765077d53581
Christophe Maymard <christophe.maymard@hotmail.com>
2020-03-17 21:36:38 +0100

    [C++] Create provider of parameters and qualifiers.

---
+++ test/PhpCode/Test/Language/Cpp/Parsing/ParametersAndQualifiersProvider.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * This file is part of the PhpCode library.
+ * 
+ * @copyright   2020, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpCode\Test\Language\Cpp\Parsing;
+
+use PhpCode\Test\Language\Cpp\CallableConceptConstraintFactory;
+use PhpCode\Test\Language\Cpp\Declarator\ParametersAndQualifiersConstraint;
+
+/**
+ * Represents the data provider related to parameters and qualifiers.
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class ParametersAndQualifiersProvider
+{
+    /**
+     * Returns a set of valid data for the provider.
+     * 
+     * @return  ValidData[]
+     */
+    public static function createValidDataSetProvider(): array
+    {
+        $dataSet = [];
+        
+        foreach (ParameterDeclarationClauseProvider::createValidDataSet() as $data) {
+            $dataSet[] = self::createPrmDeclClauseValidData($data);
+        }
+        
+        return $dataSet;
+    }
+    
+    /**
+     * Returns a set of valid data.
+     * 
+     * @return  ValidData[]
+     */
+    public static function createValidDataSet(): array
+    {
+        return self::createValidDataSetProvider();
+    }
+    
+    /**
+     * Creates a valid data for the case:
+     * ( PRM_DECL_CLAUSE )
+     * 
+     * @param   ValidData   $clauseData The parameter declaration clause data used to create the data.
+     * @return  ValidData   The created instance of ValidData.
+     */
+    private static function createPrmDeclClauseValidData(ValidData $clauseData): ValidData
+    {
+        $stream = \sprintf('(%s)', $clauseData->getStream());
+        
+        $clauseFactory = $clauseData->getConstraintFactory();
+        $callable = function () use ($clauseFactory) {
+            return new ParametersAndQualifiersConstraint(
+                $clauseFactory->createConstraint()
+            );
+        };
+        $factory = new CallableConceptConstraintFactory($callable);
+        
+        $data = new ValidData($stream, $factory);
+        $data->setName(\sprintf(
+            '( %s )', 
+            $clauseData->getName()
+        ));
+        
+        return $data;
+    }
+}
+
---
+++ test/PhpCode/Test/Unit/Test/Language/Cpp/Parsing/ParametersAndQualifiersProviderTest.php
@@ -0,0 +1,122 @@
+<?php
+/**
+ * This file is part of the PhpCode library.
+ * 
+ * @copyright   2020, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpCode\Test\Unit\Test\Language\Cpp\Parsing;
+
+use PhpCode\Test\Language\Cpp\Declarator\ParametersAndQualifiersConstraint;
+use PhpCode\Test\Language\Cpp\Parsing\ParametersAndQualifiersProvider;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Represents the unit tests for the {@see PhpCode\Test\Language\Cpp\Parsing\ParametersAndQualifiersProvider} 
+ * class.
+ * 
+ * @group   declarator
+ * @group   parsing
+ * @group   c++
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class ParametersAndQualifiersProviderTest extends TestCase
+{
+    /**
+     * Tests createValidDataSetProvider() returns an array with at least one 
+     * valid data.
+     */
+    public function testCreateValidDataSetProviderReturnsAtLeastOneValidData(): void
+    {
+        $dataSet = ParametersAndQualifiersProvider::createValidDataSetProvider();
+        self::assertTrue(\count($dataSet) > 0);
+    }
+    
+    /**
+     * Tests that createValidDataSetProvider() returns an array of valid data 
+     * that have a constraint factory that creates new instances.
+     */
+    public function testCreateValidDataSetProviderReturnsValidDataWithFactoryCreatesNewInstances(): void
+    {
+        $dataSet = ParametersAndQualifiersProvider::createValidDataSetProvider();
+        
+        foreach ($dataSet as $data) {
+            $factory = $data->getConstraintFactory();
+            $const1 = $factory->createConstraint();
+            $const2 = $factory->createConstraint();
+            self::assertNotSame($const1, $const2);
+        }
+    }
+    
+    /**
+     * Tests that createValidDataSetProvider() returns an array of valid data 
+     * that have a constraint factory that creates instance of 
+     * ParametersAndQualifiersConstraint.
+     */
+    public function testCreateValidDataSetProviderReturnsValidDataWithFactoryCreatesInstanceParametersAndQualifiersConstraint(): void
+    {
+        $dataSet = ParametersAndQualifiersProvider::createValidDataSetProvider();
+        
+        foreach ($dataSet as $data) {
+            $factory = $data->getConstraintFactory();
+            self::assertInstanceOf(
+                ParametersAndQualifiersConstraint::class, 
+                $factory->createConstraint()
+            );
+            self::assertInstanceOf(
+                ParametersAndQualifiersConstraint::class, 
+                $factory->createConstraint()
+            );
+        }
+    }
+    
+    /**
+     * Tests createValidDataSet() returns an array with at least one valid 
+     * data.
+     */
+    public function testCreateValidDataSetReturnsAtLeastOneValidData(): void
+    {
+        $dataSet = ParametersAndQualifiersProvider::createValidDataSet();
+        self::assertTrue(\count($dataSet) > 0);
+    }
+    
+    /**
+     * Tests that createValidDataSet() returns an array of valid data that 
+     * have a constraint factory that creates new instances.
+     */
+    public function testCreateValidDataSetReturnsValidDataWithFactoryCreatesNewInstances(): void
+    {
+        $dataSet = ParametersAndQualifiersProvider::createValidDataSet();
+        
+        foreach ($dataSet as $data) {
+            $factory = $data->getConstraintFactory();
+            $const1 = $factory->createConstraint();
+            $const2 = $factory->createConstraint();
+            self::assertNotSame($const1, $const2);
+        }
+    }
+    
+    /**
+     * Tests that createValidDataSet() returns an array of valid data that 
+     * have a constraint factory that creates instance of 
+     * ParametersAndQualifiersConstraint.
+     */
+    public function testCreateValidDataSetReturnsValidDataWithFactoryCreatesInstanceParametersAndQualifiersConstraint(): void
+    {
+        $dataSet = ParametersAndQualifiersProvider::createValidDataSet();
+        
+        foreach ($dataSet as $data) {
+            $factory = $data->getConstraintFactory();
+            self::assertInstanceOf(
+                ParametersAndQualifiersConstraint::class, 
+                $factory->createConstraint()
+            );
+            self::assertInstanceOf(
+                ParametersAndQualifiersConstraint::class, 
+                $factory->createConstraint()
+            );
+        }
+    }
+}
+
