09e9b75890fecbd78c85e23f1a111621512d9886
Christophe Maymard <christophe.maymard@hotmail.com>
2020-03-20 13:29:37 +0100

    [C++] Add ParametersAndQualifiersProvider::createInvalidDataSetProvider().

--- test/PhpCode/Test/Language/Cpp/Parsing/ParametersAndQualifiersProvider.php
+++ test/PhpCode/Test/Language/Cpp/Parsing/ParametersAndQualifiersProvider.php
@@ -71,5 +71,339 @@ class ParametersAndQualifiersProvider
         
         return $data;
     }
+    
+    /**
+     * Returns a set of invalid data for the provider.
+     * 
+     * @return  InvalidData[]
+     */
+    public static function createInvalidDataSetProvider(): array
+    {
+        $dataSet = [];
+        
+        $dataSet[] = self::createEmptyInvalidData();
+        $dataSet[] = self::createNoCloseInvalidData();
+        $dataSet[] = self::createNoOpenInvalidData();
+        $dataSet[] = self::createCloseBeforeOpenInvalidData();
+        
+        $prmDeclDataSet = ParameterDeclarationProvider::createValidDataSet();
+        
+        foreach ($prmDeclDataSet as $prmDeclData) {
+            $dataSet[] = self::createFirstParamMissingInvalidData($prmDeclData);
+            $dataSet[] = self::createSecondParamMissingInvalidData($prmDeclData);
+            $dataSet[] = self::createLastParamMissingInvalidData($prmDeclData);
+        }
+        
+        foreach ($prmDeclDataSet as $prmDeclData) {
+            $dataSet[] = self::createEllipsisParamInvalidData($prmDeclData);
+            $dataSet[] = self::createEllipsisCommaParamInvalidData($prmDeclData);
+            
+            $dataSet[] = self::createParamEllipsisParamInvalidData($prmDeclData);
+            $dataSet[] = self::createParamEllipsisCommaParamInvalidData($prmDeclData);
+            
+            $dataSet[] = self::createParamCommaEllipsisParamInvalidData($prmDeclData);
+            $dataSet[] = self::createParamCommaEllipsisCommaParamInvalidData($prmDeclData);
+        }
+        
+        return $dataSet;
+    }
+    
+    /**
+     * Creates an invalid data for the case:
+     * Empty string
+     * 
+     * @return  InvalidData The created instance of InvalidData.
+     */
+    private static function createEmptyInvalidData(): InvalidData
+    {
+        $stream = '';
+        $message = 'Missing "(" before "".';
+        
+        $data = new InvalidData($stream, $message);
+        
+        $data->setName('Empty string');
+        
+        return $data;
+    }
+    
+    /**
+     * Creates an invalid data for the case:
+     * No close parenthesis
+     * 
+     * @return  InvalidData The created instance of InvalidData.
+     */
+    private static function createNoCloseInvalidData(): InvalidData
+    {
+        $stream = '(';
+        $message = 'Missing ")" before "".';
+        
+        $data = new InvalidData($stream, $message);
+        
+        $data->setName('No close parenthesis');
+        
+        return $data;
+    }
+    
+    /**
+     * Creates an invalid data for the case:
+     * No open parenthesis
+     * 
+     * @return  InvalidData The created instance of InvalidData.
+     */
+    private static function createNoOpenInvalidData(): InvalidData
+    {
+        $stream = ')';
+        $message = 'Missing "(" before ")".';
+        
+        $data = new InvalidData($stream, $message);
+        
+        $data->setName('No open parenthesis');
+        
+        return $data;
+    }
+    
+    /**
+     * Creates an invalid data for the case:
+     * Close parenthesis before open parenthesis
+     * 
+     * @return  InvalidData The created instance of InvalidData.
+     */
+    private static function createCloseBeforeOpenInvalidData(): InvalidData
+    {
+        $stream = ')(';
+        $message = 'Missing "(" before ")".';
+        
+        $data = new InvalidData($stream, $message);
+        
+        $data->setName('Close parenthesis before open parenthesis');
+        
+        return $data;
+    }
+    
+    /**
+     * Creates an invalid data for the case:
+     * The first parameter is missing
+     * 
+     * @param   ValidData   $prmDeclData    The parameter declaration data use to create the data.
+     * @return  InvalidData The created instance of InvalidData.
+     */
+    private static function createFirstParamMissingInvalidData(
+        ValidData $prmDeclData
+    ): InvalidData
+    {
+        $stream = \sprintf(
+            '(,%s,%s)', 
+            $prmDeclData->getStream(), 
+            $prmDeclData->getStream()
+        );
+        $message = 'Missing ")" before ",".';
+        
+        $data = new InvalidData($stream, $message);
+        
+        $data->setName('First parameter is missing');
+        
+        return $data;
+    }
+    
+    /**
+     * Creates an invalid data for the case:
+     * The second parameter is missing
+     * 
+     * @param   ValidData   $prmDeclData    The parameter declaration data use to create the data.
+     * @return  InvalidData The created instance of InvalidData.
+     */
+    private static function createSecondParamMissingInvalidData(
+        ValidData $prmDeclData
+    ): InvalidData
+    {
+        $stream = \sprintf(
+            '(%s,,%s)', 
+            $prmDeclData->getStream(), 
+            $prmDeclData->getStream()
+        );
+        $message = 'Unexpected ",", expected decl-specifier.';
+        
+        $data = new InvalidData($stream, $message);
+        
+        $data->setName('Second parameter is missing');
+        
+        return $data;
+    }
+    
+    /**
+     * Creates an invalid data for the case:
+     * The last parameter is missing
+     * 
+     * @param   ValidData   $prmDeclData    The parameter declaration data use to create the data.
+     * @return  InvalidData The created instance of InvalidData.
+     */
+    private static function createLastParamMissingInvalidData(
+        ValidData $prmDeclData
+    ): InvalidData
+    {
+        $stream = \sprintf(
+            '(%s,%s,)', 
+            $prmDeclData->getStream(), 
+            $prmDeclData->getStream()
+        );
+        $message = 'Unexpected ")", expected decl-specifier.';
+        
+        $data = new InvalidData($stream, $message);
+        
+        $data->setName('Last parameter is missing');
+        
+        return $data;
+    }
+    
+    /**
+     * Creates an invalid data for the case:
+     * Parameter after ellipsis
+     * 
+     * @param   ValidData   $prmDeclData    The parameter declaration data use to create the data.
+     * @return  InvalidData The created instance of InvalidData.
+     */
+    private static function createEllipsisParamInvalidData(
+        ValidData $prmDeclData
+    ): InvalidData
+    {
+        $stream = \sprintf('(... %s)', $prmDeclData->getStream());
+        $message = \sprintf(
+            'Missing ")" before "%s".', 
+            $prmDeclData->getFirstTokenLexeme()
+        );
+        
+        $data = new InvalidData($stream, $message);
+        
+        $data->setName('Parameter after ellipsis');
+        
+        return $data;
+    }
+    
+    /**
+     * Creates an invalid data for the case:
+     * Parameter after ellipsis and comma
+     * 
+     * @param   ValidData   $prmDeclData    The parameter declaration data use to create the data.
+     * @return  InvalidData The created instance of InvalidData.
+     */
+    private static function createEllipsisCommaParamInvalidData(
+        ValidData $prmDeclData
+    ): InvalidData
+    {
+        $stream = \sprintf('(...,%s)', $prmDeclData->getStream());
+        $message = 'Missing ")" before ",".';
+        
+        $data = new InvalidData($stream, $message);
+        
+        $data->setName('Parameter after ellipsis and comma');
+        
+        return $data;
+    }
+    
+    /**
+     * Creates an invalid data for the case:
+     * Parameter after parameter and ellipsis
+     * 
+     * @param   ValidData   $prmDeclData    The parameter declaration data use to create the data.
+     * @return  InvalidData The created instance of InvalidData.
+     */
+    private static function createParamEllipsisParamInvalidData(
+        ValidData $prmDeclData
+    ): InvalidData
+    {
+        $stream = \sprintf(
+            '(%s ... %s)', 
+            $prmDeclData->getStream(), 
+            $prmDeclData->getStream()
+        );
+        $message = \sprintf(
+            'Missing ")" before "%s".', 
+            $prmDeclData->getFirstTokenLexeme()
+        );
+        
+        $data = new InvalidData($stream, $message);
+        
+        $data->setName('Parameter after parameter and ellipsis');
+        
+        return $data;
+    }
+    
+    /**
+     * Creates an invalid data for the case:
+     * Parameter after parameter, ellipsis and comma
+     * 
+     * @param   ValidData   $prmDeclData    The parameter declaration data use to create the data.
+     * @return  InvalidData The created instance of InvalidData.
+     */
+    private static function createParamEllipsisCommaParamInvalidData(
+        ValidData $prmDeclData
+    ): InvalidData
+    {
+        $stream = \sprintf(
+            '(%s ...,%s)', 
+            $prmDeclData->getStream(), 
+            $prmDeclData->getStream()
+        );
+        $message = 'Missing ")" before ",".';
+        
+        $data = new InvalidData($stream, $message);
+        
+        $data->setName('Parameter after parameter, ellipsis and comma');
+        
+        return $data;
+    }
+    
+    /**
+     * Creates an invalid data for the case:
+     * Parameter after parameter, comma and ellipsis
+     * 
+     * @param   ValidData   $prmDeclData    The parameter declaration data use to create the data.
+     * @return  InvalidData The created instance of InvalidData.
+     */
+    private static function createParamCommaEllipsisParamInvalidData(
+        ValidData $prmDeclData
+    ): InvalidData
+    {
+        $stream = \sprintf(
+            '(%s,... %s)', 
+            $prmDeclData->getStream(), 
+            $prmDeclData->getStream()
+        );
+        $message = \sprintf(
+            'Missing ")" before "%s".', 
+            $prmDeclData->getFirstTokenLexeme()
+        );
+        
+        $data = new InvalidData($stream, $message);
+        
+        $data->setName('Parameter after parameter, comma and ellipsis');
+        
+        return $data;
+    }
+    
+    /**
+     * Creates an invalid data for the case:
+     * Parameter after parameter, comma, ellipsis and comma
+     * 
+     * @param   ValidData   $prmDeclData    The parameter declaration data use to create the data.
+     * @return  InvalidData The created instance of InvalidData.
+     */
+    private static function createParamCommaEllipsisCommaParamInvalidData(
+        ValidData $prmDeclData
+    ): InvalidData
+    {
+        $stream = \sprintf(
+            '(%s,...,%s)', 
+            $prmDeclData->getStream(), 
+            $prmDeclData->getStream()
+        );
+        $message = 'Missing ")" before ",".';
+        
+        $data = new InvalidData($stream, $message);
+        
+        $data->setName('Parameter after parameter, comma, ellipsis and comma');
+        
+        return $data;
+    }
 }
 
--- test/PhpCode/Test/Unit/Test/Language/Cpp/Parsing/ParametersAndQualifiersProviderTest.php
+++ test/PhpCode/Test/Unit/Test/Language/Cpp/Parsing/ParametersAndQualifiersProviderTest.php
@@ -118,5 +118,15 @@ class ParametersAndQualifiersProviderTest extends TestCase
             );
         }
     }
+    
+    /**
+     * Tests createInvalidDataSetProvider() returns an array with at least 
+     * one invalid data.
+     */
+    public function testCreateInvalidDataSetProviderReturnsAtLeastOneInvalidData(): void
+    {
+        $dataSet = ParametersAndQualifiersProvider::createInvalidDataSetProvider();
+        self::assertTrue(\count($dataSet) > 0);
+    }
 }
 
