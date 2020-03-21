3ebcbf7895e632a0488708aad0228d2f7759c0bb
Christophe Maymard <christophe.maymard@hotmail.com>
2020-03-21 20:11:54 +0100

    [C++] Create a builder of identifier expression constraint double.

---
+++ test/PhpCode/Test/Language/Cpp/Expression/IdExpressionConstraintDoubleBuilder.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This file is part of the PhpCode library.
+ * 
+ * @copyright   2020, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpCode\Test\Language\Cpp\Expression;
+
+use PhpCode\Test\Language\Cpp\AbstractConceptConstraintDoubleBuilder;
+
+/**
+ * Represents a double constraint builder of the {@see PhpCode\Test\Language\Cpp\Expression\IdExpressionConstraint} 
+ * class.
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class IdExpressionConstraintDoubleBuilder extends AbstractConceptConstraintDoubleBuilder
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function getClassInterfaceName(): string
+    {
+        return IdExpressionConstraint::class;
+    }
+}
+
