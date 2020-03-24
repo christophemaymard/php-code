0fdc6e7e06eac1ed7aac4d7a33f33b1d43a19a30
Christophe Maymard <christophe.maymard@hotmail.com>
2020-03-24 13:19:05 +0100

    [C++] Create a builder of nested name specifier constraint double.

---
+++ test/PhpCode/Test/Language/Cpp/Expression/NestedNameSpecifierConstraintDoubleBuilder.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This file is part of the PhpCode library.
+ * 
+ * @copyright   2020, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpCode\Test\Language\Cpp\Expression;
+
+use PhpCode\Test\Language\Cpp\AbstractConceptConstraintDoubleBuilder;
+
+/**
+ * Represents a double constraint builder of the {@see PhpCode\Test\Language\Cpp\Expression\NestedNameSpecifierConstraint} 
+ * class.
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class NestedNameSpecifierConstraintDoubleBuilder extends AbstractConceptConstraintDoubleBuilder
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function getSubjectName(): string
+    {
+        return NestedNameSpecifierConstraint::class;
+    }
+}
+
