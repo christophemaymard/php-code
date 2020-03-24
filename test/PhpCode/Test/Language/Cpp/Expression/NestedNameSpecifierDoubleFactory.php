be0f9ccdc885766409ed25221e0b346faf711146
Christophe Maymard <christophe.maymard@hotmail.com>
2020-03-24 13:00:10 +0100

    [C++] Update factory of nested name specifier doubles.
    - Added NestedNameSpecifierDoubleFactory::createCount().
    - Added NestedNameSpecifierDoubleFactory::createCountGetNameSpecifiers().
    - Added NestedNameSpecifierDoubleFactory::buildCount().

--- test/PhpCode/Test/Language/Cpp/Expression/NestedNameSpecifierDoubleFactory.php
+++ test/PhpCode/Test/Language/Cpp/Expression/NestedNameSpecifierDoubleFactory.php
@@ -8,7 +8,10 @@
 namespace PhpCode\Test\Language\Cpp\Expression;
 
 use PhpCode\Language\Cpp\Expression\NestedNameSpecifier;
+use PhpCode\Language\Cpp\Lexical\Identifier;
 use PhpCode\Test\AbstractDoubleFactory;
+use Prophecy\Prophecy\ObjectProphecy;
+use Prophecy\Prophecy\ProphecySubjectInterface;
 
 /**
  * Represents a factory of doubles for the {@see PhpCode\Language\Cpp\Expression\NestedNameSpecifier} 
@@ -24,6 +27,58 @@ class NestedNameSpecifierDoubleFactory extends AbstractDoubleFactory
     protected function getSubjectName(): string
     {
         return NestedNameSpecifier::class;
+    }
+    
+    /**
+     * Creates a double where count() can be called.
+     * 
+     * @param   int $count    The value to return when count() is called.
+     * @return  ProphecySubjectInterface
+     */
+    public function createCount(int $count): ProphecySubjectInterface
+    {
+        $prophecy = $this->prophesizeSubject();
+        $this->buildCount($prophecy, $count);
+        
+        return $prophecy->reveal();
+    }
+    
+    /**
+     * Creates a double where count() and getNameSpecifiers() can be called.
+     * 
+     * @param   int             $count  The value to return when count() is called.
+     * @param   Identifier[]    $ids    The value to return when getNameSpecifiers() is called.
+     * @return  ProphecySubjectInterface
+     */
+    public function createCountGetNameSpecifiers(
+        int $count, 
+        array $ids
+    ): ProphecySubjectInterface
+    {
+        $prophecy = $this->prophesizeSubject();
+        $this->buildCount($prophecy, $count);
+        
+        $prophecy
+            ->getNameSpecifiers()
+            ->willReturn($ids);
+        
+        return $prophecy->reveal();
+    }
+    
+    /**
+     * Builds and adds a prophecy of count() to the specified prophecy.
+     * 
+     * @param   ObjectProphecy  $prophecy   The prophecy to build to.
+     * @param   int             $return     The value to return when count() is called.
+     */
+    private function buildCount(
+        ObjectProphecy $prophecy, 
+        int $return
+    ): void
+    {
+        $prophecy
+            ->count()
+            ->willReturn($return);
     }
 }
 
