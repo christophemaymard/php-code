81f52561173674d4b46e289f5e52ef63eb5e4c24
Christophe Maymard <christophe.maymard@hotmail.com>
2020-04-02 11:26:51 +0200

    [C++] Create a builder of pointer abstract declarator constraint double.

---
+++ test/PhpCode/Test/Language/Cpp/Declarator/PtrAbstractDeclaratorConstraintDoubleBuilder.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This file is part of the PhpCode library.
+ * 
+ * @copyright   2020, Christophe Maymard <christophe.maymard@hotmail.com>
+ * @license     http://opensource.org/licenses/MIT  MIT
+ */
+namespace PhpCode\Test\Language\Cpp\Declarator;
+
+use PhpCode\Test\Language\Cpp\AbstractConceptConstraintDoubleBuilder;
+
+/**
+ * Represents a builder of constraint double for the {@see PhpCode\Test\Language\Cpp\Declarator\PtrAbstractDeclaratorConstraint} 
+ * class.
+ * 
+ * @author  Christophe Maymard  <christophe.maymard@hotmail.com>
+ */
+class PtrAbstractDeclaratorConstraintDoubleBuilder extends AbstractConceptConstraintDoubleBuilder
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function getSubjectName(): string
+    {
+        return PtrAbstractDeclaratorConstraint::class;
+    }
+}
+
